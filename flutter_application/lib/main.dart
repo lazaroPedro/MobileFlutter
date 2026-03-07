0b57609f21ba0d9878efda142e4e992daf48c92e
Lázaro Pedro <lazaro.p806@gmail.com>
2026-03-06 22:31:34 -0300

    atividade 3

--- flutter_application/lib/main.dart
+++ flutter_application/lib/main.dart
@@ -1,35 +1,86 @@
 import 'package:flutter/material.dart';
 
+
 void main() {
-  runApp(
-    MaterialApp(
-      home: Container(
-        padding: EdgeInsets.all(8),
-        decoration: BoxDecoration(
-          color: Colors.indigo,
-          border: Border.all(color: Colors.teal, width: 8)
-        ),
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Alteração Flutter',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0; 
+
+  void _decrementar() {
+    setState(() {
+      if (_counter > 0) {
+        _counter--; 
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Aplicativo de Bolo'),
+      ),
+      body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          spacing: Checkbox.width,
-
-          children: [
-            formatarTexto("Nome: Lázaro Pedro"),
-            formatarTexto("Cidade: Guanambi"),
-            formatarTexto("Frase Curta: Hoje é 25 de fevereiro de 2025"),
-            ElevatedButton(
-              onPressed: () => print("Hoje é 25 de fevereiro de 2025"),
-              child: Text("Exibir Mensagem"),
+          children: <Widget>[
+            // Exibindo a imagem
+            Image.asset('images/bolo.jpg', width: 200, height: 200),
+            SizedBox(height: 20),
+            Text(
+              'Você pressionou o botão este número de vezes:',
+            ),
+            Text(
+              '$_counter',
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                ElevatedButton(
+                  onPressed: _decrementar, 
+                  child: Text('Decrementar'),
+                ),
+                SizedBox(width: 10),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      _counter++; 
+                    });
+                  },
+                  child: Text('Incrementar'),
+                ),
+              ],
             ),
           ],
         ),
       ),
-    ),
-  );
+    );
+  }
 }
-
-Widget formatarTexto(String texto) {
-  return Text(texto, style: TextStyle(fontSize: 20));
-}
-
